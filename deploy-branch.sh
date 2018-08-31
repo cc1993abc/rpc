a1458fdfc553f9790c7acacfcbb8c815115e3eeb
Victor.X.Qu <fs7744@hotmail.com>
2018-08-31 22:21:52 +0800

    test

---
+++ deploy-branch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "develop" ]; then
+  echo "Deploying CI..."
+  
+  export Version=$(cat version)-ci-$(date +%Y%m%d%H%M%S)
+  dotnet pack -c Release --output $PWD/artifacts/ci
+
+  dotnet nuget push artifacts/ci/*.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
+else
+  echo "Skipping CI deploy"
+fi
